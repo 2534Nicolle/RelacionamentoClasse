3a804567bb0710d75b0e233c450eebf99afcbfd5
2534Nicolle <162452766+2534Nicolle@users.noreply.github.com>
2024-10-09 15:36:22 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -2,4 +2,24 @@ package RelacionamentoClasses.Composição;
 
 public class Pessoa {
 
+	private String nome;
+	private Coracao coracao;
+	
+	public Pessoa(String nome, Coracao coracao) {
+		this.nome = nome;
+		this.coracao = coracao;
+	}
+ 
+	public String getNome() {
+		return nome;
+	}
+ 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getCoracao() {
+		return coracao.getBatimentos();
+	}
+	
 }
