b65da9ea13997c03add69dbb932c1963bbd0dee4
2534Nicolle <162452766+2534Nicolle@users.noreply.github.com>
2024-10-09 15:21:51 -0300

    Add files via upload

---
+++ Estudante.java
@@ -0,0 +1,15 @@
+package RelacionamentoClasses.Associação;
+
+public class Estudante {
+	
+	private String nome;
+	private Universidade universidade;
+	
+	public Estudante(String nome, Universidade universidade) {
+	    this.nome = nome;
+	    this.universidade = universidade;
+   }
+	
+	
+
+}
