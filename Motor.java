5a1310e9bc262eb6d72649a5c741fc39757eeec3
2534Nicolle <162452766+2534Nicolle@users.noreply.github.com>
2024-10-09 15:26:57 -0300

    Add files via upload

---
+++ Motor.java
@@ -0,0 +1,21 @@
+package RelacionamentoClasses.Agregacao;
+
+public class Motor {
+	
+	private String tipo;
+	
+	public Motor(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
---
+++ Principal.java
@@ -0,0 +1,12 @@
+package RelacionamentoClasses.Agregacao;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+	
+	Motor motor = new Motor("V8");
+	Carro carro = new Carro("Mustang", motor);
+	
+	System.out.println("Carro é: " + carro.getModelo() + "\nMotor: " + motor.getTipo());
+}
+}
