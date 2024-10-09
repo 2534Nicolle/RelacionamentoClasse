5000bc10a0da3d9bead480cb1ca5f8553349cfef
2534Nicolle <162452766+2534Nicolle@users.noreply.github.com>
2024-10-09 15:35:51 -0300

    Update Coracao.java

--- Coracao.java
+++ Coracao.java
@@ -1,5 +1,21 @@
 package RelacionamentoClasses.Composição;
 
 public class Coracao {
-
+	
+private int batimentos;
+	
+	public Coracao(int batimentos) {
+		this.batimentos = batimentos;
+	}
+ 
+	public int getBatimentos() {
+		return batimentos;
+	}
+ 
+	public void setBatimentos(int batimentos) {
+		this.batimentos = batimentos;
+	}
+	
+	
+ 
 }
